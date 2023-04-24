af4cf2ea9d5a7a33dcb63897e2bbb972a90cbdb7
Juan Huete <jrios9836@gmail.com>
2023-04-24 02:43:52 -0600

    'xd'

--- application.py
+++ application.py
@@ -1,13 +1,33 @@
 import os
 
-from flask import Flask
-from flask_socketio import SocketIO, emit
+from flask import Flask,render_template
+from flask_socketio import emit, join_room, leave_room, SocketIO
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
+#socketio = SocketIO(app)
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return "Project 2: TODO"
+    return render_template('index.html')
+
+
+@socketio.on("join_room")
+def entrar(room):
+    # Ingresamos al usuario a la room
+    join_room(room)
+
+    # Emitimos un aviso a los usuarios conectados a la room, exceptuando a la persona que se unio
+    emit('mensaje', f'Un usuario ha entrado a la sala {room}', broadcast=False, include_self=False, to=room)
+
+    # Mandamos una respuesta al evento emit del cliente
+    return room
+
+
+@socketio.on("message") 
+def message(data):
+  print(data)
+  emit("message", data["message"],  broadcast=False, include_self=True, to=data["room"])
+    
